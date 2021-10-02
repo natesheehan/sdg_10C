06f1c1123e11fc8d701ae554c127c9be5dd0edef
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-02 01:45:26 +0100

    add data

--- main.r
+++ main.r
@@ -1,2 +1,3 @@
+data = read.csv("data/API_BM.TRF.PWKR.CD.DT_DS2_en_csv_v2_3012027.csv",skip = 4)
 
-url = "https://api.worldbank.org/v2/en/indicator/BM.TRF.PWKR.CD.DT?downloadformat=csv"
+data_corridors = read.csv("data/36d1ed87-9a2d-41da-8eac-c1c25d45fa65_Data.csv")
