10b7548d80e3f09b273a493a1affeafa20c1cd8a
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-02 22:40:24 +0100

    plot updates

--- main.r
+++ main.r
@@ -1,30 +1,37 @@
+# Librarys ----------------------------------------------------------------
 library(WDI)
 library(stringr)
 library(tidyverse)
 
-
-# Query world bank data on remittances  -----------------------------------
-WDIsearch('remit')
-dat = WDI(indicator = 'BM.TRF.MGR.CD',
-          start = 2020,
-          end = 2020)
-
-dat = WDI(indicator = 'BM.TRF.MGR.CD')
-
-
 # Read world bank remitance corridor data ----------------------------------------------------
 #### notes: this data is not avaliable from the WorldBank R oackage WDI
+tags = c(" 0-3%", " 3-5%", " 5-10%", "10% and above") # create tags for bin labels
 #### the data has been downloaded from: https://databank.worldbank.org/source/remittance-prices-worldwide-(corridors)
 data_corridors = read.csv("data/36d1ed87-9a2d-41da-8eac-c1c25d45fa65_Data.csv")
-colnames(data_corridors)[1] = "Sending.Countries.Name"
-
-# Clean that data ---------------------------------------------------------
-#### all this data is messy, the format is suited for excel worksheets rather than machine readability
-
 colnames(data_corridors)[1] = "Sending.Countries.Name" # rename column because its called some funky shit
+data_corridors[data_corridors == ".."] = 0
 
-tags = c("0-3", "3-5", "5-10", "10 and above") # create tags for bin labels
+data_corridors[, 7:41] <- sapply(data_corridors[, 7:41], as.numeric)
+data = data_corridors %>% filter(X2020Q3..YR2020Q3. != "..") %>%
+  mutate('2011' = (X2011Q1..YR2011Q1. + X2011Q3..YR2011Q3.) / 2) %>%
+  mutate('2012' = (X2012Q1..YR2012Q1. + X2012Q3..YR2012Q3.) / 2) %>%
+  mutate('2013' = (X2013Q1..YR2013Q1. + X2013Q1..YR2013Q1.) / 2) %>%
+  mutate('2014' = (X2014Q1..YR2014Q1. + X2014Q3..YR2014Q3.) / 2) %>%
+  mutate('2015' = (X2015Q1..YR2015Q1. + X2015Q3..YR2015Q3.) / 2) %>%
+  mutate('2016' = (X2016Q1..YR2016Q1. + X2016Q3..YR2016Q3.) / 2) %>%
+  mutate('2017' = (X2017Q1..YR2017Q1. + X2017Q3..YR2017Q3.) / 2) %>%
+  mutate('2018' = (X2018Q1..YR2018Q1. + X2018Q3..YR2018Q3.) / 2) %>%
+  mutate('2019' = (X2019Q1..YR2019Q1. + X2019Q3..YR2019Q3.) / 2) %>%
+  mutate('2020' = (X2020Q1..YR2020Q1. + X2020Q3..YR2020Q3.) / 2)
 
+  # find top 10 send/recieve countries
+  sending_countries = as.data.frame(table(sort(data$Sending.Countries.Name))) %>%
+  arrange(desc(Freq))
+
+receiving_countries = as.data.frame(table(data$Receiving.Countries..Name)) %>%
+  arrange(desc(Freq))
+
+# Tidy DF for GGplot ------------------------------------------------------
 data_corridors_min = data_corridors %>%
   select(Sending.Countries.Name,
          Receiving.Countries..Name,
@@ -43,21 +50,32 @@ data_corridors_min = data_corridors %>%
       value >= 10 ~ tags[4]
     )
   )  %>% filter(
-    send == "United Kingdom" |
+    send == "United States" |
+      send == "United Kingdom"  |
+      send == "Germany" |
+      send == "Italy"  |
+      send == "Saudi Arabia"  |
+      send == "Australia"  |
       send == "France"  |
-      send == "Saudi Arabia" |
       send == "Canada"  |
-      send == "United States"  | send == "Australia"
+      send == "Russian Federation" |
+      send == "South Africa"
   )
 
 data_corridors_min = data_corridors_min %>% filter(
-  recieve == "Philippines" |
+  recieve == "India" |
     recieve == "China" |
+    recieve == "Philippines" |
+    recieve == "Bangladesh" |
     recieve == "Pakistan" |
+    recieve == "Viet Nam" |
     recieve == "Sri Lanka" |
-    recieve == "Somalia"  | recieve == "Nepal"
+    recieve == "Nigeria"  |
+    recieve == "Indonesia" |
+    recieve == "Egypt"
 )
 
+#### Calculate level summary
 receive_levels = data_corridors_min %>%
   filter(complete.cases(.)) %>%
   group_by(recieve) %>%
@@ -72,31 +90,25 @@ send_levels = data_corridors_min %>%
   arrange(-num) %>%
   pull(send)
 
-
+# mutate levels into dataframe
 df = data_corridors_min %>%
   mutate(recieve = factor(recieve, levels = receive_levels),
          send = fct_rev(factor(send, levels = send_levels))) %>%
   filter(complete.cases(send))
 
-
-df$bins = as.character(df$bins)
-df$bins[is.na(df$bins)] = "No data"
-df$bins = as.factor(df$bins)
-df$bins <-
-  factor(df$bins,
-         levels = c("0-3", "3-5", "5-10", "10 and above", "No data"))
-
-
+# text col
 textcol = "grey40"
+
+# Plot --------------------------------------------------------------------
 p = ggplot(df, aes(x = recieve, y = send, fill = bins)) +
   geom_tile(colour = "white", size = 0.2) +
-  scale_colour_manual(values = c("purple", "blue", "green")) +
+  scale_colour_manual(values = c("red", "blue", "green")) +
   guides(fill = guide_legend(title = "Average cost of sending remittances between countries")) +
   labs(
     x = "Countries Where Capital is Received",
     y = "Countries Where Capital is Sent",
-    title = "World Bank Remittance Corridors Data\n2020 Q3 (Latest)",
-    caption = "Remittance costs vary between sending and receiving country corridors.\nThe SDG target aims to bring all corridor costs to below\n5% of the amount remitted."
+    title = "World Bank Remittance Corridors Data 2020 Q3 (Latest)",
+    caption = "Remittance costs vary between sending and receiving country corridors.The SDG target aims to bring all corridor costs to below 5% of the amount remitted."
   ) +
   #coord_fixed()+
   theme_grey(base_size = 10) +
@@ -110,12 +122,14 @@ p = ggplot(df, aes(x = recieve, y = send, fill = bins)) +
       face = "bold"
     ),
     legend.key.height = grid::unit(1.4, "cm"),
-    legend.key.width = grid::unit(0.2, "cm"),
+    legend.key.width = grid::unit(0.8, "cm"),
     axis.text.x = element_text(size = 10, colour = textcol),
     axis.text.y = element_text(vjust = 0.2, colour = textcol),
     axis.ticks = element_line(size = 0.4),
     plot.background = element_blank(),
     panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
     plot.margin = margin(0.7, 0.4, 0.1, 0.2, "cm"),
     plot.title = element_text(
       colour = textcol,
@@ -125,8 +139,4 @@ p = ggplot(df, aes(x = recieve, y = send, fill = bins)) +
     )
   )
 
-
-
 p
-## read amount paid
-####data = read.csv("data/API_BM.TRF.PWKR.CD.DT_DS2_en_csv_v2_3012027.csv",skip = 4)
