e631785f11afd695c10b2538aead4b6852eea4d8
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-04 00:54:07 +0100

    reprod

--- main.r
+++ main.r
@@ -53,13 +53,14 @@ table(data$sdg_target) # 0 = no 1 = yes
 post_sdg = data %>% filter(year >= 2016) %>%
   filter(sdg_target == 0) ### SDG false
 
+# frequency tables
 send_sdg =  as.data.frame(table(post_sdg$Sending.Countries.Name)) %>% arrange(desc(Freq))
 rec_sdg =  as.data.frame(table(post_sdg$Receiving.Countries..Name)) %>% arrange(desc(Freq))
 
 summary(post_sdg$value)
 
 # UpperQ
-upperQ = post_sdg %>% filter(value >= 10.198) %>% filter(year == 2016)
+upperQ = post_sdg %>% filter(value >= 10.198) %>% filter(year == 2020)
 
 # calculate countries
 countries_rec = as.data.frame(upperQ$Receiving.Countries..Name)
@@ -77,6 +78,8 @@ countries = countries %>%
           method = 'osm', # calculate osm country point
           lat = latitude ,
           long = longitude)
+
+saveRDS(countries, "data/countries.RDS")
 
 countries = st_as_sf(countries, coords = c("longitude","latitude"), crs = "WGS84")
 #Set DF to SF
