f7d18af2bf43a60751636755172473f314971a64
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-04 15:10:54 +0100

    add install_pkgs.r

---
+++ scripts/install_pkgs.r
@@ -0,0 +1,21 @@
+####
+####
+# Aim: Install and load packages required for analysis --------------------
+####
+####
+
+# list CRAN packages used
+pkgs = c("od", # generate OD lines
+         "tidyverse", # for data cleaning
+         "stringr", # for string manipulation
+         "tidygeocoder", # OSM api used to calculate OD between corridors
+         "sf" ) # simple feature handling
+
+# Install pkgs not yet installed
+installed_packages = pkgs %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(pkgs[!installed_packages])
+}
+
+# Load pkgs
+invisible(lapply(pkgs, library, character.only = TRUE))
