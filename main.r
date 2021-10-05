2ebf01b34a08f50008763b60c2e852ff4a49d43c
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-05 20:31:41 +0100

    stlye main script

--- main.r
+++ main.r
@@ -1,4 +1,6 @@
 # Run analysis ------------------------------------------------------------
 source("scripts/install_pkgs.r")
+
 source("scripts/data_cleaning.r")
+
 source("scripts/sdg_analysis.r")
