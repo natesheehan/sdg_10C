161b115a195cf71e1b38fabecc6cc5e394083dc5
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-06 21:14:19 +0100

    clean main

--- main.r
+++ main.r
@@ -1,8 +1,14 @@
 # Run analysis ------------------------------------------------------------
 source("scripts/install_pkgs.r")
 
+
+# Clean World Bank Data for Statistical Inference -------------------------
 source("scripts/data_cleaning.r")
 
+
+# Run functions for historical remitance ----------------------------------
 source("scripts/functions.r")
 
+
+# Run SDG maps analysis ---------------------------------------------------
 source("scripts/sdg_analysis.r")
