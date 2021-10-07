3dd5437571db6d0782ee4ca00c9787b8b1508f4f
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-07 23:02:01 +0100

    update plot for cvc

--- scripts/sdg_analysis.r
+++ scripts/sdg_analysis.r
@@ -5,32 +5,48 @@
 ####
 
 #### As ranked by the GermanWatch 2011-2019 climate vulnerable countries index
-climate_vulnerable_countries = c(
-  "Puerto Rico",
-  "Myanmar",
-  "Haiti",
-  "Philippines",
-  "Mozambique",
-  "Bahamas",
-  "Bangladesh",
-  "Pakistan",
-  "Thailand",
-  "Nepal"
+climate_vulnerable_countries = as.data.frame(
+  c(
+    "Puerto Rico",
+    "Myanmar",
+    "Haiti",
+    "Philippines",
+    "Mozambique",
+    "Bahamas",
+    "Bangladesh",
+    "Pakistan",
+    "Thailand",
+    "Nepal"
+  )
 )
+colnames(climate_vulnerable_countries) = "country"
 
-
-climate_vulnerable_countries_19 = c(
-  "Mozambique",
-  "Zimbabwe",
-  "Bahamas",
-  "Japan",
-  "Malawi",
-  "Afganistan",
-  "India",
-  "South Sudan",
-  "Niger",
-  "Boliva"
+# 2019 list
+climate_vulnerable_countries_19 = as.data.frame(
+  c(
+    "Mozambique",
+    "Zimbabwe",
+    "Bahamas",
+    "Japan",
+    "Malawi",
+    "Afghanistan",
+    "India",
+    "South Sudan",
+    "Niger",
+    "Bolivia"
+  )
 )
+colnames(climate_vulnerable_countries_19) = "country"
+
+cvc = rbind(climate_vulnerable_countries,climate_vulnerable_countries_19)
+cvc = as.data.frame(unique(cvc$country))
+colnames(cvc) = "country"
+
+cvc = cvc %>%
+  geocode(country,
+          method = 'osm', # calculate osm country point
+          lat = latitude ,
+          long = longitude)
 
 
 # Calculate and visualise year_average ------------------------------------
@@ -73,27 +89,32 @@ ggplot(data = na.omit(data), aes(x = year, y = value,  group = 1)) +
     x = "Year",
     y = "Remitance percent value of sending $200 (or equivelent in local currency)",
     title = "Global Remitance Corridors Tax Averages",
-    subtitle = paste0("Data from the World bank records ",n_countries," countries in its World Wide Remittance Database")
+    subtitle = paste0(
+      "Data from the World bank records ",
+      n_countries,
+      " countries in its World Wide Remittance Database"
+    )
   ) + theme_modern_rc()
 
 
 
-post_sdg = data %>% filter(year >= 2016)
-hist(post_sdg$value)
-summary(post_sdg$value)
-#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
-# 0.075   4.808   6.626   7.382   9.175  26.971
 # frequency tables total
 send_sdg =  as.data.frame(table(post_sdg$Sending.Countries.Name)) %>% arrange(desc(Freq))
 rec_sdg =  as.data.frame(table(post_sdg$Receiving.Countries..Name)) %>% arrange(desc(Freq))
 
+post_sdg = data %>% filter(year >= 2016)
+hist(post_sdg$value, main = "Histogram of Post SDG (2016) Remittance Values", xlab = "value (%)")
+summary(post_sdg$value)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.075   4.808   6.626   7.382   9.175  26.971
 upperQ = post_sdg %>% filter(value >= 9.175) %>% filter(year == 2020) %>% arrange((value))
+upperq_share = upperQ %>% group_by(Sending.Countries.Name) %>% tally() %>% arrange(desc(n)) %>% rename(country = Sending.Countries.Name)
+
+
 lowerQ = post_sdg %>% filter(value <= 4.808) %>% filter(year == 2020) %>% arrange((value))
 
-
-post_sdg = data %>% filter(year >= 2016) %>% filter(sdg_target == 0)
-summary(post_sdg$value)
-
+# post_sdg = data %>% filter(year >= 2016) %>% filter(sdg_target == 0)
+# summary(post_sdg$value)
 
 # Visualise network of upper and lower Q ----------------------------------
 if (file.exists("data/countries.RDS")) {
@@ -116,21 +137,30 @@ countries = st_as_sf(countries,
 countries = st_as_sf(countries)
 st_crs(countries) = "WGS84"
 
+mapview::mapview(countries)
+
 # transform data for OD calculation
 upperQ_sf = upperQ %>% select(Sending.Countries.Name, Receiving.Countries..Name, value)
-desire_lines_od = od_to_sf(upperQ_sf, countries)
+upperQ_sf_od = od_to_sf(upperQ_sf, countries)
+
+mapview::mapview(upperQ_sf_od)
+
+lowerQ_sf = lowerQ %>% select(Sending.Countries.Name, Receiving.Countries..Name, value)
+lowerrQ_sf_od = od_to_sf(lowerQ_sf, countries)
+
+mapview::mapview(lowerrQ_sf_od)
+
 
 library(mapview)
 #View OD connection
-mapview(desire_lines_od)
 
 library(tmap)
-tmap_mode("plot")
+tmap_mode("view")
 tm_basemap(leaflet::providers$Stamen.TonerLite) +
-  tm_shape(desire_lines_od) +
+  tm_shape(lowerrQ_sf_od) +
   tm_lines(
     palette = "plasma",
-    breaks = c(10, 12, 14, 16, 18, 20),
+    breaks = c(0, 1, 2, 3, 4, 5),
     lwd = "value",
     scale = 9,
     title.lwd = "%",
@@ -143,4 +173,19 @@ tm_basemap(leaflet::providers$Stamen.TonerLite) +
   tm_layout(legend.bg.alpha = 0.5,
             legend.bg.color = "white")
 
-
+tm_basemap(leaflet::providers$Stamen.TonerLite) +
+  tm_shape(upperQ_sf_od) +
+  tm_lines(
+    palette = "plasma",
+    breaks = c(9, 11, 13, 15, 17, 19, 30),
+    lwd = "value",
+    scale = 9,
+    title.lwd = "%",
+    alpha = 0.5,
+    col = "value",
+    title = "Remittance value (%)",
+    legend.lwd.show = TRUE
+  ) +
+  tm_scale_bar() +
+  tm_layout(legend.bg.alpha = 0.5,
+            legend.bg.color = "white")
